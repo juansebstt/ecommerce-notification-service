40861a3d360fa3288ccc7a84a0b8407d2fb8f4ad
Juan Sebastian Ibarra <juanseibarra011@gmail.com>
2024-10-08 12:28:32 -0500

    first commit

---
+++ src/main/java/com/ecommercenotificationservice/EcommerceNotificationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ecommercenotificationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceNotificationServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcommerceNotificationServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ecommercenotificationservice/EcommerceNotificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommercenotificationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceNotificationServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
